4daafbd8ad40bed06b94cf22ff95c1f330b2a72b
Berdysh <a.berdyshev@netbox.ru>
2022-03-25 18:52:26 +0300

    brain-games: fix brain-games

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,5 @@
-import readlineSync from 'readline-sync';
-
 function greet() {
-  console.log('Welcome to the Brain Games!');
-
-  const userName = readlineSync.question('May I have your name? ');
-
-  console.log(`Hi ${userName}!`);
+  console.log('Hello, Tirion');
 }
 
 export default greet;
